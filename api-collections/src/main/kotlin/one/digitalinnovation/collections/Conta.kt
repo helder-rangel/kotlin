3b5ec67cb81ebdd1558f53f61c03290d6cdbd51c
helder-rangel <helder.rangel@academico.ifpb.edu.br>
2021-07-17 10:09:48 -0300

    Add functions deposito and saque

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.collections
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String,
+    val conta: String,
+    var saldo: BigDecimal
+        ){
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
