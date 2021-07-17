657c89a4fdb603b660b3ce23a30d5990b4755812
helder-rangel <helder.rangel@academico.ifpb.edu.br>
2021-07-17 09:49:53 -0300

    Update to apply the private modifier to set function of cpf property

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -3,6 +3,11 @@ package one.digitalinnovation.collections
 class Pessoa {
     var nome: String = "Helder"
     var cpf: String = "123.123.123-11"
+    private set
+
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
 }
 
 fun main(){
@@ -10,4 +15,5 @@ fun main(){
 
     println(helder.nome)
     println(helder.cpf)
+    println(helder.Endereco().rua)
 }
